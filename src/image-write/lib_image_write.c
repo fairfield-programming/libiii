52485cfd9aa1dcbf8c4ed6d3c550b0794b4d5b79
William McGonagle <wpmcgonagle@gmail.com>
2023-01-28 19:00:47 -0500

    Added Write Features

--- src/image-write/lib_image_write.c
+++ src/image-write/lib_image_write.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void test(void)
+bool condense_point_value(char *body, int index, int point_size, int value_offset, int old_value_size, int new_value_size)
 {
-    printf("Test successfully called");
+
+    return false;
 }
--- src/image-write/lib_image_write.h
+++ src/image-write/lib_image_write.h
@@ -1,1 +1,1 @@
-void test(void);
+bool condense_point_value(char *body, int index, int point_size, int value_offset, int old_value_size, int new_value_size);
